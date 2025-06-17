7e0911a49f4aca42392c54e2e37b7a808fa338a8
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2025-06-17 12:07:47 -0500

    coNTUNUACION AJUSTES

--- src/components/BackgroundDecoration.jsx
+++ src/components/BackgroundDecoration.jsx
@@ -1,11 +1,11 @@
 const BackgroundDecoration = () => {
     return (
         <div className="background-decoration">
-            <img src="/assets/Group.svg" alt="Decoración 1" style={{ top: "0", left: "-10%" }} />
-            <img src="/assets/Group-1.svg" alt="Decoración 2" style={{ top: "10%", right: 0 }} />
-            <img src="/assets/Group-2.svg" alt="Decoración 3" style={{ bottom: "0", left: "-10%" }} />
-            <img src="/assets/Group-3.svg" alt="Decoración 4" style={{ bottom: "-5%", right: "-10%" }} />
-            <img src="/assets/Group-4.svg" alt="Decoración 5" style={{ top: "50%", left: "30%" }} />
+            {/* <img src="/assets/Group.svg" alt="Decoración 1" style={{ top: "0", left: "-10%" }} /> */}
+            <img src="/assets/Group-1.svg" alt="Decoración 2" style={{ top: 0, right: "0%" }} />
+            {/* <img src="/assets/Group-2.svg" alt="Decoración 3" style={{ bottom: -50, left: 0 }} /> */}
+            {/* <img src="/assets/Group-3.svg" alt="Decoración 4" style={{ bottom: "-3%", left: "-5%" }} /> */}
+            <img src="/assets/Group-4.svg" alt="Decoración 5" style={{ bottom: -40, left: -100 }} />
         </div>
     )
 }
