e88e78d3a8301e8f0c99f2a5ff816376ae3bc36f
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2025-06-18 16:00:57 -0500

    ajustes mobile 3.5

--- src/components/BackgroundDecoration.jsx
+++ src/components/BackgroundDecoration.jsx
@@ -3,8 +3,8 @@ const BackgroundDecoration = () => {
         <div className="background-decoration">
             <img src="/assets/Group.svg" alt="Decoración 1" style={{ top: 0, left: -50, rotate: "100deg" }} />
             <img src="/assets/Group-1.svg" alt="Decoración 2" style={{ top: -20, right: -120, rotate: "100deg" }} />
-            <img src="/assets/Group-2.svg" alt="Decoración 3" style={{ bottom: -50, right: -60 }} />
-            <img src="/assets/Group-3.svg" alt="Decoración 4" style={{ bottom: -70, right: 0}} />
+            <img src="/assets/Group-2.svg" alt="Decoración 3" style={{ bottom: -60, right: -60 }} />
+            <img src="/assets/Group-3.svg" alt="Decoración 4" style={{ bottom: -80, right: 0}} />
             <img src="/assets/Group-4.svg" alt="Decoración 5" style={{ bottom: -70, left: -120 }} />
         </div>
     )
